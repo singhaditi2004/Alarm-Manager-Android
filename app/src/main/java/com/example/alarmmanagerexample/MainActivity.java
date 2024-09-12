1cb6dd4ab129e11302d413d806ed57c083bb1902
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-09-12 07:48:46 +0530

    Alarm Manager added

---
+++ app/src/main/java/com/example/alarmmanagerexample/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.alarmmanagerexample;
+
+import android.app.AlarmManager;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText text;
+    AppCompatButton button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        text=findViewById(R.id.editText);
+        button.findViewById(R.id.button);
+        text.requestFocus();
+        AlarmManager am=(AlarmManager) getSystemService(ALARM_SERVICE);
+        am.set();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
