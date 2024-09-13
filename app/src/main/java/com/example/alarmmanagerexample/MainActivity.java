2a6f5d5703fe078d3a5ad9fff287bbcf8f330635
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-09-13 12:32:07 +0530

    It is completed

--- app/src/main/java/com/example/alarmmanagerexample/MainActivity.java
+++ app/src/main/java/com/example/alarmmanagerexample/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.alarmmanagerexample;
 
 import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
@@ -13,17 +16,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     EditText text;
-    AppCompatButton button;
+    AppCompatButton btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        text=findViewById(R.id.editText);
-        button.findViewById(R.id.button);
+        text = findViewById(R.id.editText);
+        btn=findViewById(R.id.button);
         text.requestFocus();
-        AlarmManager am=(AlarmManager) getSystemService(ALARM_SERVICE);
-        am.set();
+        AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String f = text.getText().toString();
+                long n = Long.parseLong(f);
+                Intent intent=new Intent(MainActivity.this,BroadCastR.class);
+                PendingIntent i=PendingIntent.getBroadcast(MainActivity.this,1,intent,PendingIntent.FLAG_MUTABLE);
+                am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + (n * 1000),i);
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
